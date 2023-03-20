3ea58885ffd0084ab508ace5da666fb82ecb6fa2
Sriballo93 <sebariba93@gmail.com>
2023-03-20 15:34:51 +0100

    ft conectback

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,42 @@
-import './Login.css';
+import { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 
-import React from 'react';
+import { UserContext } from '../../Context/UserContext';
+import { API } from '../../Services/API';
 
 const Login = () => {
-  return <main>Login</main>;
+  const { register, handleSubmit } = useForm();
+  let navigate = useNavigate();
+  const { login } = useContext(UserContext);
+  const [error, setError] = useState(null);
+
+  const formSubmit = (formData) => {
+    API.post('/users/login', formData)
+      .then((res) => {
+        if (res.status === 200) {
+          login(res.data.userName, res.data.token);
+          navigate('/');
+        }
+      })
+      .catch((error) => {
+        setError(error);
+      });
+  };
+
+  return (
+    <main>
+      <h2>Login</h2>
+      {error !== null && <h2>{error}</h2>}
+      <form onSubmit={handleSubmit(formSubmit)}>
+        <label htmlFor="userName">Username</label>
+        <input type="text" id="userName" name="userName" {...register('userName')} />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" {...register('password')} />
+        <button type="submit">Login</button>
+      </form>
+    </main>
+  );
 };
 
 export default Login;
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
 
 import App from './App';
+import { UserContextProvider } from './Context/UserContext';
 import About from './Pages/About/About';
 import ActivityDetail from './Pages/ActivityDetail/ActivityDetail';
 import Home from './Pages/Home/Home';
@@ -16,18 +17,20 @@ import Sections from './Pages/Sections/Sections';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="/sections" element={<Sections />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/activityDetail" element={<ActivityDetail />} />
-          <Route path="/about" element={<About />} />
-        </Route>
-      </Routes>
+    <BrowserRouter basename="/">
+      <UserContextProvider>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<Home />} />
+            <Route path="/sections" element={<Sections />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/activityDetail" element={<ActivityDetail />} />
+            <Route path="/about" element={<About />} />
+          </Route>
+        </Routes>
+      </UserContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
