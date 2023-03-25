3aa6f8cabf6e5412a286c1a3c008f6ea78a1a806
Christian Alonso <christianalonso.r10@gmail.com>
2023-03-25 20:10:25 +0100

    carousel

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
 
 import App from './App';
+import ProtectedRoutes from './Components/ProtectedRoutes/ProtectedRoutes';
 import { CityContextProvider } from './Context/CityContext';
 import { ThemeContextProvider } from './Context/ThemeContext';
 import { UserContextProvider } from './Context/UserContext';
@@ -16,7 +17,6 @@ import Login from './Pages/Login/Login';
 import Profile from './Pages/Profile/Profile';
 import Register from './Pages/Register/Register';
 import Sections from './Pages/Sections/Sections';
-import ProtectedRoutes from './Components/ProtectedRoutes/ProtectedRoutes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
